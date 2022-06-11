6d1d21720f17bcaf488c5e045ad7b7e1b5feb4c2
renatoseb <renatorodriguezllanos@gmail.com>
2022-06-10 21:39:22 -0500

    indexes

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,18 @@ from flask import jsonify
 from flask_cors import CORS
 import postgres
 import mongodb_search
+import time
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route('/python-req/<text>/<top_k>', methods=['GET'])
 def python_request(text, top_k):
-    #response = jsonify(postgres.get_postgres_topk(text, int(top_k)))
-    response = jsonify({'message': 'success'})
+    
+    start = time.time()
+    # response = search()
+    end = time.time()
+    time = round(end - start, 6)
     return response
 
 @app.route('/postgres-req/<text>/<top_k>', methods=['GET'])
